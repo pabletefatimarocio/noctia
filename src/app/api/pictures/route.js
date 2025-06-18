2100ed08642b50218c3fbf30d137aa021e6d0086
pabletefatimarocio <pabletefatima9@gmail.com>
2025-06-18 16:07:41 -0500

    route pictures, styles home

---
+++ src/app/api/pictures/route.js
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+
+const pictures =[
+    {
+        id: 1,
+        img:"https://res.cloudinary.com/dllyvg3us/image/upload/v1749769182/Fomapan_100__22b_of_39_ycehpf.jpg",
+        film: "fomapan 400",
+        width: 5794,
+        heigth: 3863
+    },
+    {
+        id: 2,
+        img:"https://res.cloudinary.com/dllyvg3us/image/upload/v1749769189/Fomapan_100__27_of_39_pxghif.jpg",
+        film: "fomapan 400",
+        width: 5794,
+        heigth: 3863
+    },
+    {
+        id: 3,
+        img:"https://res.cloudinary.com/dllyvg3us/image/upload/v1749769188/Fomapan_100__25_of_39_hlqhbd.jpg",
+        film: "fomapan 400",
+        width: 5794,
+        heigth: 3863
+    },
+    {
+        id: 4,
+        img:"https://res.cloudinary.com/dllyvg3us/image/upload/v1749769188/Fomapan_100__16_of_39_fzgalq.jpg",
+        film: "fomapan 400",
+        width: 5794,
+        heigth: 3863
+    },
+    {
+        id: 5,
+        img:"https://res.cloudinary.com/dllyvg3us/image/upload/v1749769192/Fomapan_100__1_of_39_ewvfwu.jpg",
+        film: "fomapan 400",
+        width: 5794,
+        heigth: 3863
+    },
+    {
+        id: 6,
+        img:"https://res.cloudinary.com/dllyvg3us/image/upload/v1749769176/IMG_20231103_152416_027_1_riqaku.jpg",
+        film: "fomapan 100",
+        width: 720,
+        heigth: 843
+    },
+    
+   
+]
